0a536ebd45d45e40ead2bf26e78157e6bb127953
Rishabh Sagar <rishabh.sagar.eee19@itbhu.ac.in>
2024-01-09 17:53:18 +0530

    first commit

--- src/pages/Watch2.jsx
+++ src/pages/Watch2.jsx
@@ -82,14 +82,14 @@ function WatchPage2() {
 					</div>
  
 					<iframe
-                className="h-[40vh] w-[90vw] mt-[40vh] md:mt-0 md:h-full md:w-full"
+                className="h-[40vh] w-[90vw] mt-[40vh] md:mt-0 mx-auto md:h-full md:w-full"
                 src={videoId}
                 allowFullScreen
             ></iframe>
                 </div>
             ):(
 				<iframe
-                className="h-[40vh] w-[90vw] mt-[40vh] md:mt-0 md:h-full md:w-full"
+                className="h-[40vh] w-[90vw] mt-[40vh] md:mt-0 mx-auto md:h-full md:w-full"
                 src={videoId}
                 allowFullScreen
             ></iframe>
